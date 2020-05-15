727056766f08512581b9ea8ddc3affc0c5e11d67
Elias Olofsson <tfy17eon@cs.umu.se>
2020-05-16 00:35:06 +0200

    Started on base structure for shallow waters.

--- shallow_water.c
+++ shallow_water.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
+#include <math.h>
 
 
-#define CELLCOUNT 50
-
-
-
+/*
+ * Shallow Water Equations 2D. Hardcoded for square regions with square cells.
+ * 
+ * 2020-05-15 
+ *
+ * Author: Elias Olofsson (tfy17eon@cs.umu.se)
+ *
+ */
 
 
 int main (void) 
 {	
 	// Defining constant parameters
-	int const model_length = 1;
-	double const lambda = 0.05;
-	double const T = 0.5;
-	double const g = 9.82;
+	int const CELLCOUNT = 50;			// Cell count per side.
+	double const model_length = 1;			// Length of side.
+	double const lambda = 0.05;			// lambda == dt/dx
+	double const T = 0.5;				// Time to solve.
+	double const g = 9.82;				// Gravity acceleration.
+
+	// Calc the following parameters	
+	double const dx = model_length/CELLCOUNT;	// dx == dy
+	double const dt = lambda*dx;			// dt
+	int const Ntimesteps = floor(T/dt);		// Nr of timesteps.
 	
-	double dx = model_length/CELLCOUNT;
-	double dt = lambda*dx;
-	int Ntimesteps = 30;
+	// Vectors of cell centers
+	double x_cc [CELLCOUNT];
+	double y_cc [CELLCOUNT];
+	for (int i = 0; i < CELLCOUNT; i++) {
+		x_cc[i] = dx/2 + i*dx; 
+		y_cc[i] = dx/2 + i*dx;
+	}
+	
+	// Initial conditions	
+	
+	
+	
 	
 
-
-
+	
 	return 0;
 }
